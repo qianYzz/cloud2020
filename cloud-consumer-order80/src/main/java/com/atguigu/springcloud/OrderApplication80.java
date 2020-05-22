62416cfacead4858b1fdeba5f5a060d1af10de59
liuy <liuyang7374@gmail.com>
2020-05-23 00:38:07 +0800

    修改文件名称

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderApplication80.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderApplication80.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OrderMain80 {
+public class OrderApplication80 {
     public static void main(String[] args) {
-        SpringApplication.run(OrderMain80.class,args);
+        SpringApplication.run(OrderApplication80.class,args);
     }
 
 }
