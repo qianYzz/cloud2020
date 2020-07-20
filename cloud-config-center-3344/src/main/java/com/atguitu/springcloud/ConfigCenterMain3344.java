5e82faf864cb5222d2c9baa994d20cc8af2cd859
liuy <liuy>
2020-07-20 08:55:29 +0800

    Merge branch 'master' of F:\gitworkspice\cloud2020 with conflicts.

--- cloud-config-center-3344/src/main/java/com/atguitu/springcloud/ConfigCenterMain3344.java
+++ cloud-config-center-3344/src/main/java/com/atguitu/springcloud/ConfigCenterMain3344.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * @description:
+ * @author: liuy
+ * @time: 2020/7/14 18:48
+ */
+
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigCenterMain3344 {
--- cloud-config-client-3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
+++ cloud-config-client-3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -1,14 +1,18 @@
 package com.atguigu.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+/**
+ * @description:
+ * @author: liuy
+ * @time: 2020/7/14 19:44
+ */
 @RestController
 @RefreshScope
 public class ConfigClientController {
-
     @Value("${config.info}")
     private String configInfo;
 
